e61b6efc173a0e18ecd60d4c14a29c46a59346b3
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-16 10:31:04 -0500

    fleshed out index.js in routes and api

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thoughtsRoutes');
+const userRoutes = require('./userRoutes');
+
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+
+module.exports = router;
