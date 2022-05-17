00f860b238c8867ba97d3a1ef9f81dbbcf8ea6b6
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-16 21:05:32 -0500

    built out routes for thoughts and users

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createFriend,
+    deleteFriend
+
+} = require('../../controllers/userControllers.js');
+
+
+router.route('/').get(getUsers).post(createUser);
+
+
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+    
+router
+    .route('/:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend)
+
+
+module.exports = router;
