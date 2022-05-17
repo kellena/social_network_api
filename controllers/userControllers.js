f7a749c6a2a43ed9a00f00cb191f183a6b438de7
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-16 20:44:21 -0500

    wrote create and delete for friend

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -76,9 +76,39 @@ module.exports = {
 
             .then(()=> res.json({message: 'User and thoughts have been deleted'}))
             .catch((err)=> res.status(500).json(err));
-            
+
     },
 
-    // write out create/delete friend
+    createFriend(req,res){
+
+        User.findOneAndUpdate({_id: req.params.userId}, {$addToSet: {friends: req.params.friendId}}, {runValidators: true, new: true})
+   
+                .then((userData)=>
+
+                    !userData
+                        ? res.status(404).json({message: "No user found with this ID"})
+                        : res.json(userData)
+
+            )
+
+        .catch((err)=> res.status(500).json(err));  
+
+    },
+
+    deleteFriend(req,res){
+
+        User.findOneAndUpdate({_id: req.params.userId}, {$pull: {friends: req.params.friendId}}, {runValidators: true, new: true})
+            
+            .then((userData)=>
+
+                !userData
+                    ? res.status(404).json({message: "No user found with this ID"})
+                    : res.json(userData)
+
+        )
+        
+        .catch((err)=> res.status(500).json(err)); 
+    
+    },
 
 };
